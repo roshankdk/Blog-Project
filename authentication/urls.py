2d42b3b7e9f0a411fbbf2671cb4aada89b836e68
Roshankdk <roshankdk.27@gmail.com>
2024-05-08 19:39:20 +0545

    Made some changes

--- authentication/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.admin import register
-from django.urls import path
-from authentication import views
-urlpatterns = [
-    path("login/",views.login,name='login'),
-    path("register/",views.register,name='register'),
-]
