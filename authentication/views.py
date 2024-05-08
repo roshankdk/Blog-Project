bb94a33ff6d8727251ab1c5a502901efb2b11914
Roshan Kdk <135725198+roshankdk@users.noreply.github.com>
2024-05-08 19:27:15 +0545

    Delete authentication directory

--- authentication/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import HttpResponse, render
-
-# Create your views here.
-def login(request):
-    return HttpResponse("<h1>This is the login page</h1>".encode('utf-8'))
-
-def register(request):
-    return HttpResponse("<h1>This is the register page</h1>".encode('utf-8'))
